eb1505d98dafc15bc032a3c9494be414c3a6a831
Fonsah Pageo <ashprincepageo@gmail.com>
2022-08-30 18:39:52 +0100

    work update

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ALl Projects</title>
+    <title>All Projects</title>
     <style>
         body ul, body h1{
             margin: 2em;
