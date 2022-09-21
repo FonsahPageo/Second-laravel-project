d510fea7f001b9722684110e43be54d77321fc58
Fonsah Pageo <ashprincepageo@gmail.com>
2022-09-21 11:20:05 +0100

    work update, better encapsulation

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -11,15 +11,27 @@ use App\Models\Project;
 class ProjectTasksController extends Controller
 {
     public function store(Project $project){
-        $attributes = request() -> validate(['description' => 'required']);
-
-        $project -> addTask($attributes);
+        $project -> addTask(request() -> validate(['description' => 'required']));
         return back();        
     }
 
     public function update(Task $task){
-        $task -> update([
-            'completed' => request() -> has('completed')
-        ]);
+        // $task -> complete(request() -> has('completed'));
+        // same as above
+        // $task -> update([
+        //     'completed' => request() -> has('completed')
+        // ]);
+
+        // if(request() -> has('copleted')){
+        //     $task -> complete();
+        // } else{
+        //     $task -> incomplete();
+        // }
+
+        // request() -> has('completed') ? $task -> complete() : $task -> incomplete;
+
+        $method = request() -> has('completed') ? 'complete' : 'incomplete';
+        $task -> $method();
+        return back();
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function complete($completed = true){
+        $this -> update(compact('completed'));
+    }
+
+    public function incomplete(){
+        $this -> complete(false);
+    }
 
     public function project(){
         return $this -> belongsTo(Project::class);
